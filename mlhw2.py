fc049602638d644450178dc5c49df2e9a2df8f17
joseph1027 <joseph102712080104@gmail.com>
2017-11-25 01:43:56 -0600

    Add files via upload
    output to a txt file

--- mlhw2.py
+++ mlhw2.py
@@ -33,17 +33,17 @@ def build_tree(root,train_data,divide_column):
 	root.value = train_data[median_index]
 	root.divide = divide_column
 
-	print('divide_column:',divide_column)
+	#print('divide_column:',divide_column)
 	if(divide_column==10):
 		divide_column = 2
 	else:
 		divide_column = divide_column + 1
-	print('value:',root.value)
-	print('data_len:',data_len)
-	print('left:')
+	#print('value:',root.value)
+	#print('data_len:',data_len)
+	#print('left:')
 	if data_len<=1 :
 		return root
-	lcnt=0
+	"""lcnt=0
 	rcnt=0
 	for i in range(0,int(data_len/2)):
 		lcnt = lcnt+1
@@ -54,7 +54,7 @@ def build_tree(root,train_data,divide_column):
 		print(train_data[j])
 		rcnt = rcnt+1
 		print(rcnt)
-	print('\n')
+	print('\n')"""
 	root.left_child = build_tree(root.left_child,train_data[:median_index],divide_column)
 	if data_len > 2:
 		root.right_child = build_tree(root.right_child,train_data[median_index+1:],divide_column)
@@ -68,42 +68,46 @@ def reset_node(current_node):
 		reset_node(current_node.right_child)
 
 def test(root,train_data):
+	text_file = open("output.txt","w")
 	test_data = []
 	test_data.extend(train_data[0:36])
 	#print(test_data)
 	origin_class = None
 	predict_class = None
+	dis = None
 	result = [[] for v in range(3)]
 	class_name = ['cp','im','pp','imU','om','omL','imL','imS']
 	for number in [1,5,10,100]:# k
 		correct = 0
-		result = [[] for v in range(3)]	
+		result = [[] for v in range(0,3)]	
 		for data_index in range(0,len(test_data)):#0~35
 			query_data = test_data[data_index]
 			origin_class = query_data[11]
 			reset_node(root)
 			result_table = {'cp':0,'im':0,'pp':0,'imU':0,'om':0,'omL':0,'imL':0,'imS':0}
-			for k in range(0,number):#number+1
-				NN,predict_class = knn(root,query_data)
+			for k in range(0,number):
+				nearest_neighbor,predict_class = knn(root,query_data)
 				result_table[predict_class] = result_table[predict_class] + 1
 				if(data_index >= 0 and data_index <= 2):
-					result[data_index].append(NN)
+					result[data_index].append(nearest_neighbor)
 
 			max_vote = 0
-			for hash_index in range(len(class_name)):
-				if result_table[class_name[hash_index]] > max_vote:
-					max_vote = result_table[class_name[hash_index]]
-					final_predict_class = class_name[hash_index]
+			for i in range(0,len(class_name)):
+				name = class_name[i]
+				if result_table[name] > max_vote:
+					max_vote = result_table[name]
+					final_predict_class = class_name[i]
 
-			print('Origin:',origin_class,' Predict:',final_predict_class)
+			#print('Origin:',origin_class,' Predict:',final_predict_class)
 			if origin_class == final_predict_class:
 				correct = correct + 1
-		print('A KNN is Done', data_index,' accuracy is ',float(correct/36.0))
-		for cnt in range(3):
+		text_file.write('KNN accuracy: ' + str(float(correct/36.0)) + '\n')
+		for cnt in range(0,3):
 			for out_index in range(len(result[cnt])):
-				print(result[cnt][out_index]+' ',end="")
-			print('')
-
+				text_file.write(str(result[cnt][out_index]) + ' ')
+			text_file.write('\n')
+		text_file.write('\n')
+	text_file.close()		
 					
 def knn(root,query_data):
 	cur_node = root
@@ -112,7 +116,7 @@ def knn(root,query_data):
 	record = []
 	while cur_node is not None:
 		record.append(cur_node)
-		cur_distance = distance(query_data,cur_node)
+		cur_distance = distance(query_data,cur_node.value)
 		cur_divide = cur_node.divide
 		if cur_distance < min_distance and cur_node.searched == 0:
 			nearest = cur_node
@@ -125,39 +129,33 @@ def knn(root,query_data):
 	while record:
 		parent_node = record.pop()
 		cur_divide = parent_node.divide
-		cur_distance = distance(query_data,parent_node)
-		if parent_node.left_child is None and parent_node.right_child is None:
-			if cur_distance < min_distance and parent_node.searched == 0:
-				nearest = parent_node
-				min_distance = cur_distance
-		else:
-			if abs(float(query_data[cur_divide])-float(parent_node.value[cur_divide])) < min_distance:
-				if query_data[cur_divide] < parent_node.value[cur_divide]:
-					cur_node = parent_node.right_child
-				else:
-					cur_node = parent_node.left_child
-				if cur_node is not None:
-					while cur_node:
-						record.append(cur_node)
-						cur_distance = distance(query_data,cur_node)
-						cur_divide = cur_node.divide
-						if cur_distance < min_distance and cur_node.searched == 0:
-							nearest = cur_node
-							min_distance = cur_distance
-						if query_data[cur_divide] < cur_node.value[cur_divide]:
-							cur_node = cur_node.left_child
-						else:
-							cur_node = cur_node.right_child
+		if abs(float(query_data[cur_divide])-float(parent_node.value[cur_divide])) < min_distance:
+			if query_data[cur_divide] < parent_node.value[cur_divide]:
+				cur_node = parent_node.right_child
+			else:
+				cur_node = parent_node.left_child
+			if cur_node is not None:
+				while cur_node is not None:
+					record.append(cur_node)
+					cur_distance = distance(query_data,cur_node.value)
+					cur_divide = cur_node.divide
+					if cur_distance < min_distance and cur_node.searched == 0:
+						nearest = cur_node
+						min_distance = cur_distance
+					if query_data[cur_divide] < cur_node.value[cur_divide]:
+						cur_node = cur_node.left_child
+					else:
+						cur_node = cur_node.right_child
 	nearest.searched = 1
-	nearest_id = nearest.value[0]
+	nearest_index = nearest.value[0]
 	nearest_class = nearest.value[11]
-	return nearest_id,nearest_class
+	return nearest_index,nearest_class
 						
 
 
-def distance(query_data,cur_node):
+def distance(query_data,cur_node_data):
 	a = query_data
-	b = cur_node.value
+	b = cur_node_data
 	value = 0
 	for i in range (2,11):#2~10
 		value = value + math.pow(a[i] - b[i],2)
@@ -187,17 +185,14 @@ if __name__ == '__main__':
 	global counter
 	counter = 0
 
-	print('TREE GO!')
-	tree_go(root)
-	print (counter)
+	#print('TREE GO!')
+	#tree_go(root)
+	#print (counter)
 	test(root,training_data)
 	#print('T 0:',training_data[0])
 	#print('RL :',root.left_child.value)
 
 	#print(distance(training_data[0],root.left_child))
-	#test(root,training_data)
-
-	#test(root,train_data)
 
 
 
