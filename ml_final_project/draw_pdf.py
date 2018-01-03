14993416421d8487c262a52cd4d258573566a42e
joseph1027 <joseph102712080104@gmail.com>
2018-01-03 09:30:38 +0800

    Create draw_pdf.py
    maker done.
    manufacture year done.

---
+++ ml_final_project/draw_pdf.py
@@ -0,0 +1,72 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import neighbors
+from sklearn import tree 
+
+def preprocessing():
+    df = pd.read_csv('car.csv')
+    
+    #delete unnecessary data columns
+    df = df.drop(df.index[0]) #drop the label, say the first row , NOT IN PLACE
+    del df['body_type']
+    del df['color_slug']
+    del df['stk_year']
+    del df['date_created']
+    del df['date_last_seen']
+    df = df.dropna() #drop row with na value, NOT IN PLACE
+    
+    #print(df)
+    car_name = df[['maker']]
+    car_name.dtypes.value_counts()
+    return df
+
+def draw_PDF(df): #draw the pdf of each column
+    df = df.mask(df.astype(object).eq('None')).dropna()
+    sample_df = df.sample(n=1000)
+    
+    #print(sample_df)
+    maker_count = sample_df.groupby(['maker']).size()
+    
+    year_count = sample_df.groupby(['manufacture_year']).size()
+    #print(year_count)
+    
+    makers = maker_count.keys()
+    maker_list = []
+    count_list = []
+    summ = 0
+    for i in range(len(makers)):
+        maker_list.append(makers[i])
+        count_list.append(maker_count[i])
+        summ = summ + maker_count[i]
+        
+    years = year_count.keys()
+    year_count = year_count.tolist()
+    #print(years)
+    year_list = []
+    year_count_list = []
+    year_sum = 0
+    for i in range(len(years)):
+        year_list.append(years[i])
+        year_count_list.append(year_count[i])
+        year_sum = year_sum + year_count[i]
+    
+    
+    #print(year_list)
+    #print(year_count_list)
+    
+    plt.figure(figsize=(30,5))
+    plt.plot(maker_list,count_list/summ)
+    plt.tight_layout()
+    plt.show()
+    
+    plt.figure(figsize=(10,5))
+    #print(type(year_list))
+    #print(type(year_count_list))
+    plt.plot(year_list,year_count_list)
+    plt.xticks(np.arange(min(year_list), max(year_list)+1, 5.0))
+    plt.tight_layout()
+    plt.show()
+        
+if __name__ == '__main__':
+    draw_PDF(preprocessing())
